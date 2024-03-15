c69a7b950ec71dba069948e96fb2d65c81300466
parvadha <parvadha.ramar@gmail.com>
2024-03-15 12:16:24 +0530

    movie app created new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Msg name="Nagavalli" pic="https://img.freepik.com/free-photo/vibrant-colors-nature-close-up-wet-purple-daisy-generated-by-artificial-intellingence_25030-63819.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1710460800&semt=ais"/>
+     <Msg name="Bhavani" pic="https://c8.alamy.com/comp/TACMXH/three-red-rose-flower-buds-and-petals-from-a-vase-TACMXH.jpg"/>
+     <Msg name="Devi" pic="https://images.pexels.com/photos/87452/flowers-background-butterflies-beautiful-87452.jpeg?cs=srgb&dl=pexels-pixabay-87452.jpg&fm=jpg"/>
+
     </div>
   );
+}
+
+//component name first letter must be capital
+//return JSX elements
+function Msg(props)
+{
+  //Js code
+  //JSX starts
+  return(
+    <div className='msg'>
+      <img src={props.pic} className='profile'/>
+      <h3>Hello {props.name} ⭐👌🍿!!!</h3>
+    </div>
+  )
+  //JSX ends
 }
 
 export default App;
